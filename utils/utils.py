bd37626dee63144afa4a317f68d60ae9e4838e64
EoinNM <molloy@cbs.mpg.de>
2018-03-20 15:46:10 +0100

    Initial commit

---
+++ utils/utils.py
@@ -0,0 +1,11 @@
+
+def mkdir_path(path):
+    import os
+    import errno
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+    return path
