cca2f4120a7d090851baaec3857aba47577ef5c9
EoinNM <33571955+EoinNM@users.noreply.github.com>
2018-04-06 11:11:19 +0200

    Add files via upload

--- variables/variables.py
+++ variables/variables.py
@@ -14,5 +14,15 @@ population= ["AL3T", "BTBT", "GAUT", "IN3T", "MCLT", "PAKT", "SICT", "UC2T", "ZL
              "FL9T", "HS8X", "KTIT", "NT6T", "RMAX", "TO3T", "WSKT", "BSVT", "FN3T", "IL2T",
              "KTKT", "OK7T", "SATX", "TSFT", "ZJ4T"]
 
+group_A= ["BJKT" "BMQX" "BSVT" "CE2T" "DF6T" "FL8T" "FL9T" "GAUT" "GCPT" "GJHT" 
+	  "GJJT" "HSBX" "JG2T" "KA3X" "KA6X" "KTKT" "KTIT" "NAAT" "NG4T" "PAKT" 
+	  "PMUT" "PV3T" "RKHT" "SICT" "SJDX" "SLPT" "SM9Y" "SSNX" "TCET" "UC2T" 
+	  "VA9T" "VL3T" "VM9T" "VS7T"]
+
+group_B= ["AL3T" "BCQT" "BDPT" "BM4X" "BMNX" "BTBT" "DF7T" "FE5T" "FN3T" "GJFT" 
+	  "GJGT" "IL2T" "IN3T" "KCST" "VL4T" "KSBX" "MCLT" "MCMT" "MCNT" "NL5T" 
+	  "NT6T" "OK7T" "PAMT" "PJGT" "RMAY" "SATX" "TO3T" "TSFT" "WSKT" "ZJ4T" 
+	  "ZL2T"]
+
 
 test_population = ["KSBX", "HS8X", "ZJ4T"]
