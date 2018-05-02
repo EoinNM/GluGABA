9f146fc6a5b8365b9053124698ba2f14ea31dc9e
EoinNM <33571955+EoinNM@users.noreply.github.com>
2018-05-02 18:00:50 +0200

    Add files via upload

--- 01_segmentation.py
+++ 01_segmentation.py
@@ -30,8 +30,13 @@ def Segment_T1(workspace, population, days):
             csf_mask = os.path.join(anatomical_dir, 'c3ANATOMICAL.nii')
             
             print ' Now-Thresholding & Binarising tissue classes for subject %s' % subject
-            # threshold/binarise tissue classes
-            os.system('fslmaths %s -add %s -add %s -thr 0.5 -bin brain_mask' % (gm_mask, wm_mask, csf_mask))            
+            # threshold/binarise tissue classes to create mask
+            os.system('fslmaths %s -add %s -add %s -thr 0.5 -bin brain_mask' % (gm_mask, wm_mask, csf_mask))
+
+	    # threshold binarise indivdiual tissue classes
+	    os.system('fslmaths gm_mask -thr 0.5 -bin GM')
+	    os.system('fslmaths wm_mask -thr 0.5 -bin WM') 
+	    os.system('fslmaths csf_mask -thr 0.5 -bin CSF')      
 
             print 'Now Making Mask for subject %s' % subject
             # create brain mask for GM, WM, CSF
