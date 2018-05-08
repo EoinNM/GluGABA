d2d93dde2ade2a6222914fa6472460111b77081b
EoinNM <33571955+EoinNM@users.noreply.github.com>
2018-05-08 16:26:46 +0200

    Update 01_segmentation.py

--- 01_segmentation.py
+++ 01_segmentation.py
@@ -38,7 +38,7 @@ def Segment_T1(workspace, population, days):
             os.system('fslmaths %s -add %s -add %s -thr 0.5 -bin brain_mask' % (gm_mask, wm_mask, csf_mask))
             
             os.chdir(anatomical_dir)
-            #binarise tissue masks for voxel count calculation - BE IN FSL ENVIRONMENT
+            #binarise individual tissue masks and save outputs in anatomical dir - BE IN FSL ENVIRONMENT
             os.system('fslmaths %s -thr 0.5 -bin GM' % (gm_mask))
             os.system('fslmaths %s -thr 0.5 -bin WM' % (wm_mask))
             os.system('fslmaths %s -thr 0.5 -bin CSF' % (csf_mask))
