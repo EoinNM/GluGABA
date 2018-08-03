0f4ca94f48bdb05fb6f53dfe86e5d3318d7c69d0
EoinNM <33571955+EoinNM@users.noreply.github.com>
2018-08-03 16:53:51 +0200

    Add files via upload

--- 03_create_voxel_masks.py
+++ 03_create_voxel_masks.py
@@ -1,9 +1,3 @@
-"""
-Created on Mon Apr 16 16:33:53 2018
-
-@author: molloy
-"""
-
 import os
 from os import *
 from utils.utils import *
@@ -26,29 +20,34 @@ def create_masks(population, workspace, days, voxels):
                 print '============================================================================================'
                 print '     %s. Obtaining RDA Geometry and Creating Binarised Voxel Mask for %s, %s, %s' %(count, subject, day, voxel)
                 print '============================================================================================'    
+                
+                if not os.path.isfile(os.path.join(workspace, 'DATA', subject, 'SVS', day, voxel, 'RDA', voxel)):
+                
+                    #work dir
+                    sub_dir = os.path.join(workspace)
+                    
+                    #set paths & define files for RDA_TO_NIFTI MatLab script              
+                    t1Path = os.path.join(sub_dir, 'DATA', subject, 'ANATOMICAL', day + '/')
+                    t1File = "ANATOMICAL.nii"
+                    RPath = os.path.join(sub_dir, 'DATA', subject, 'SVS', day, voxel, 'RDA', voxel + '/')
+                    RFile = '%s.rda' %(voxel)
+                    
+                    #create new output_dir
+                    mkdir_path(os.path.join(sub_dir, 'DATA', subject, 'SVS', day, voxel, 'voxel_masks'))
+                    mask_dir = os.path.join(sub_dir, 'DATA', subject, 'SVS', day, voxel, 'voxel_masks')
+                    
+                    #MatLab/SPM in os.system
+                    os.system("matlab -nodesktop -nosplash -softwareopengl -noFigureWindows -r \"RDA_TO_NIFTI_e(\'%s\', \'%s\', \'%s\', \'%s\') ; quit;\"" %(t1Path, t1File, RPath, RFile))
     
-                #work dir
-                sub_dir = os.path.join(workspace)
-                
-                #set paths & define files for RDA_TO_NIFTI MatLab script              
-                t1Path = os.path.join(sub_dir, subject, 'ANATOMICAL', day + '/')
-                t1File = "ANATOMICAL.nii"
-                RPath = os.path.join(sub_dir, subject,'SVS', day, voxel, 'RDA', voxel + '/')
-                RFile = '%s.rda' %(voxel)
-                
-                #create new output_dir
-                mkdir_path(os.path.join(sub_dir, subject, 'SVS', day, voxel, 'voxel_masks'))
-                mask_dir = os.path.join(sub_dir, subject, 'SVS', day, voxel, 'voxel_masks')
-                
-                #MatLab/SPM in os.system
-                os.system("matlab -nodesktop -nosplash -r \"RDA_TO_NIFTI(\'%s\', \'%s\', \'%s\', \'%s\') ; quit;\"" %(t1Path, t1File, RPath, RFile))
+                    #move voxel masks from T1 path directory to voxel_mask dir
+                    for file in os.listdir(t1Path):
+                        if 'rda' in file and '%s' %voxel in file:
+                            shutil.move(os.path.join(t1Path, file), mask_dir)
+    
+                        elif 'coord' in file:
+                            shutil.move(os.path.join(t1Path, file), mask_dir)
+                        
+                else:
+                    print 'Mask already exists - Check dir to see it'
 
-                #move voxel masks from T1 path directory to voxel_mask dir
-                for file in os.listdir(t1Path):
-                    if 'rda' in file and '%s' %voxel in file:
-                        shutil.move(os.path.join(t1Path, file), mask_dir)
-
-                    elif 'coord' in file:
-                        shutil.move(os.path.join(t1Path, file), mask_dir)
-
-create_masks(test_population, workspace, days, voxels)
+create_masks(population, workspace, days, voxels)
